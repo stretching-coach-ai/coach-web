49aa25c195b9d215fbabdb8ddd29bb77a35d94d3
summit1123 <ruggy245@naver.com>
2025-02-27 01:20:18 +0900

    remove :root directory node_modules

--- node_modules/hugeicons-react/dist/esm/defaultAttributes.js
+++
@@ -1,7 +0,0 @@
-/**
- * @license hugeicons-react v0.2.0
- *
- * See the LICENSE file in the root directory of this source tree.
- */
-
-var e={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",strokeWidth:1.5,strokeLinecap:"round",strokeLinejoin:"round"};export{e as default};
